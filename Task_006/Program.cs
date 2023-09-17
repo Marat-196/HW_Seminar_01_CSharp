d9d20152b11a6fcdac8ea7ab1cbf547ed7152ac6
Marat <zhumataev.marat@bk.ru>
2023-09-17 20:43:45 +0500

    add Task 006

---
+++ Task_006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA % 2 == 0)
+{
+    Console.WriteLine($"{numA} -> Да");
+}
+else
+{
+    Console.WriteLine($"{numA} -> Нет");
+}
