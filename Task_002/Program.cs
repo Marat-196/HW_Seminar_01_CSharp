1a99f0d6f849f697d96cbb85ef763a53d1e680da
Marat <zhumataev.marat@bk.ru>
2023-09-17 20:14:42 +0500

    add Task 02

---
+++ Task_002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine($"max = {numA}, min = {numB}");
+}
+else
+{
+    Console.WriteLine($"max = {numB}, min = {numA}");
+}
