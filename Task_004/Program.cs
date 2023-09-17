c9caa5ab49c39b461a6e3654d8fab99c07060f3f
Marat <zhumataev.marat@bk.ru>
2023-09-17 20:35:17 +0500

    add Task 004

---
+++ Task_004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int mx = numA;
+
+if (numA > mx) mx = numA;
+if (numB > mx) mx = numB;
+if (numC > mx) mx = numC;
+
+Console.WriteLine($"max = {mx}");
