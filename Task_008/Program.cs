817cca672b25fd0ad51c38ff7d18774119571237
Marat <zhumataev.marat@bk.ru>
2023-09-17 20:59:02 +0500

    add Task 008

---
+++ Task_008/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count += 1;
+}
